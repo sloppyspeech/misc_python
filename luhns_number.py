735c57fab2b111af5aa992b1593d222f1369269e
sloppyspeech <sloppy.speech@gmail.com>
2018-09-06 11:27:52 +0530

    Modified as Class

--- luhns_number.py
+++ luhns_number.py
@@ -7,10 +7,35 @@ add all the digits 2+6+4+2+0+7+9+8 = 40
 Multiply the sum by 9 :40*9 = 360 
 tenths digit is the luhns number i.e. 0 in this case
 '''
-def get_check_digit(p_inp):
-    num=[ int(d) for d in str(p_inp)]
-    even_digits=f.reduce(lambda x,y:x+y,num[::2])
-    odd_digits=f.reduce(lambda x,y:x+y,[i+i if i+i <= 9 else i+i-9 for i in num[1::2]])
-    return(((even_digits+odd_digits)*9)%10)
+class LuhnsNumber(object):
+    '''
+        number passed to get_check_digit will generate and return check digit
+        number passed to is_valid will check if the last digit of the number is valid 
+          check digit based on Luhns Algoritm
 
-print(get_check_digit(23420894))
+    '''
+    def __init__(self,p_inp):
+        self.p_inp=p_inp
+
+    def get_check_digit(self):
+        num=[ int(d) for d in str(self.p_inp)]
+        even_digits=f.reduce(lambda x,y:x+y,num[::2])
+        odd_digits=f.reduce(lambda x,y:x+y,[i+i if i+i <= 9 else i+i-9 for i in num[1::2]])
+        return(((even_digits+odd_digits)*9)%10)
+
+    def is_valid(self):
+        check_digit=str(self.p_inp)[-1]
+        self.p_inp=str(self.p_inp)[:len(str(self.p_inp))-1]
+        if int(check_digit) == self.get_check_digit() :
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return "LuhnsNumber({0})".format(self.p_inp)
+
+if __name__=='__main__':
+    luhns_num=LuhnsNumber(53461861341123)
+    print(luhns_num.get_check_digit())
+    luhns_num=LuhnsNumber(534618613411238)
+    print(luhns_num.is_valid())
--- test_luhn.py
+++ test_luhn.py
@@ -4,9 +4,18 @@ import luhns_number as ln
 class TestLuhn(unittest.TestCase):
     
     def test_luhn(self):
-        self.assertEqual(ln.get_check_digit(53461861341123),4)
-        self.assertTrue(ln.get_check_digit(35693803564380)==9)
-        
+        luhns_num=ln.LuhnsNumber(53461861341123)
+        self.assertEqual(luhns_num.get_check_digit(),4)
+        luhns_num=ln.LuhnsNumber(534618613411234)
+        self.assertTrue(luhns_num.is_valid())
+        luhns_num=ln.LuhnsNumber(35693803564380)
+        self.assertEqual(luhns_num.get_check_digit(),9)
+        luhns_num=ln.LuhnsNumber(356938035643809)
+        self.assertTrue(luhns_num.is_valid())
+        luhns_num=ln.LuhnsNumber(35693803564380)
+        self.assertFalse(luhns_num.get_check_digit()==7)
+        luhns_num=ln.LuhnsNumber(356938035643807)
+        self.assertFalse(luhns_num.is_valid())
 
 if __name__ == '__main__':
     unittest.main()

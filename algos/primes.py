d953a73de8b07bc7f1d3d5e160e36234f029ab12
sloppyspeech <sloppy.speech@gmail.com>
2018-09-26 17:41:53 +0530

    Primes By Sieve of Eratosthenes

---
+++ algos/primes.py
@@ -0,0 +1,52 @@
+import argparse
+
+def is_prime(inp=0):
+    if(inp==0 or inp==1 or inp==2 or inp==3 or inp==5 or inp==7 ):
+        return True
+    elif (inp%2==0 or inp%3==0 or inp%5==0 or inp%7==0):
+        return False
+    else:
+        idx=5
+        while (idx*idx < inp) :
+            if (inp%idx == 0 ) or (inp %(idx+2)==0 ):
+                return False
+            idx=idx+6
+        return True
+
+def get_primes_by_sieve(input_num):
+    """"Get List of All primes by using sieve of eratosthenes"
+    
+    Arguments:
+        input_num {int} -- Number upto which the list of primes to be calculated
+    
+    Returns:
+        [list] -- [list of all nums below the input value]
+    """
+    inp=[i for i in range(input_num)]
+    inp.pop(0)
+    inp.pop(0)
+    inp1=inp[:]
+    idx=0
+    while idx < len(inp):
+        try:
+            divisor=inp[idx]
+        except IndexError:
+            print("Index Error **********")
+            print("{0}".format(idx))
+            print(inp)
+            print(len(inp))
+            exit(1)
+        for i,v in enumerate(inp,divisor):
+            if v%divisor==0 and v!=divisor:
+                inp1.remove(v)
+        idx=idx+1
+        inp=inp1[:]
+    return inp
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('input_num',type=int)
+    args=parser.parse_args()
+    sieve = get_primes_by_sieve(args.input_num)
+    print("List of Primes below than {0} => {1}".format(args.input_num,sieve))
+ 
